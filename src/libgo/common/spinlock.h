4fcec432d3f06a6ac943e46be1783a2a8ab88cc0
brinkqiang <brink.qiang@gmail.com>
2020-07-06 18:17:19 +0800

    add

--- src/libgo/common/spinlock.h
+++ src/libgo/common/spinlock.h
@@ -11,7 +11,11 @@ struct BooleanFakeLock
 
     ALWAYS_INLINE void lock()
     {
-        while (!locked_) locked_ = true;
+        while (!locked_)
+        {
+            locked_ = true;
+        }
+
         DebugPrint(dbg_spinlock, "lock");
     }
 
@@ -23,11 +27,16 @@ struct BooleanFakeLock
     ALWAYS_INLINE bool try_lock()
     {
         bool ret = !locked_;
-        if (ret) locked_ = true;
+
+        if (ret)
+        {
+            locked_ = true;
+        }
+
         DebugPrint(dbg_spinlock, "trylock returns %s", ret ? "true" : "false");
         return ret;
     }
-    
+
     ALWAYS_INLINE void unlock()
     {
         assert(locked_);
@@ -39,9 +48,9 @@ struct BooleanFakeLock
 // 性能高于LFLock2
 struct LFLock
 {
-    std::atomic_flag flag;
+    std::atomic_flag flag = ATOMIC_FLAG_INIT;
 
-    LFLock() : flag{false}
+    LFLock()
     {
     }
 
@@ -54,26 +63,33 @@ struct LFLock
     {
         return !flag.test_and_set(std::memory_order_acquire);
     }
-    
+
     ALWAYS_INLINE void unlock()
     {
         flag.clear(std::memory_order_release);
     }
 };
 
-struct FakeLock {
+struct FakeLock
+{
     void lock() {}
-    bool is_lock() { return false; }
-    bool try_lock() { return true; }
+    bool is_lock()
+    {
+        return false;
+    }
+    bool try_lock()
+    {
+        return true;
+    }
     void unlock() {}
 };
 
 // atomic_bool可能不是无锁
 struct LFLock2
 {
-    std::atomic_bool state;
+    std::atomic_bool state = false;
 
-    LFLock2() : state{false}
+    LFLock2()
     {
     }
 
@@ -86,10 +102,11 @@ struct LFLock2
     {
         return !state.exchange(true, std::memory_order_acquire);
     }
-    
+
     ALWAYS_INLINE void unlock()
     {
-        if (!state.exchange(false, std::memory_order_release)) {
+        if (!state.exchange(false, std::memory_order_release))
+        {
             throw std::logic_error("libgo.spinlock unlock exception: state == false");
         }
     }
